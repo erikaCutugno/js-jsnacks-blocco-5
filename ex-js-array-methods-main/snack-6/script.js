d6a006584fbbeebca3fc764194a64007c2e287cb
erikaCutugno <erikucciapop@gmail.com>
2024-12-17 12:40:45 +0100

    feat: exe 6

--- ex-js-array-methods-main/snack-6/script.js
+++ ex-js-array-methods-main/snack-6/script.js
@@ -12,3 +12,8 @@ const zucchine = [
 ];
 
 // A partire dall'array fornito, crea due array. Uno con le zucchine più lunghe di almeno 15cm. L'altro con le restanti.
+const zucchineLunghe = zucchine.filter((elm) => elm.length >= 15)
+console.log(zucchineLunghe)
+
+const zucchineCorte = zucchine.filter((elm) => elm.length < 15)
+console.log(zucchineCorte)
